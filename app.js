dd400ddc9d47f7ed2e41e90faf04cacc4b748f93
Perry Huang <perryhuang2015@gmail.com>
2025-08-25 19:42:34 -0400

    feat: move and implement features outside of app

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
-const { body, validationResult } = require("express-validator");
 const path = require("node:path");
+const passport = require("./controllers/passportController");
 const indexRouter = require("./routes/indexRouter");
+const expressSession = require("./controllers/sessionController");
 
 const app = express();
-app.use(express.urlencoded({ extended: true })); // make post work and have a body
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
+
+app.use(expressSession);
+app.use(passport.session());
+app.use(express.urlencoded({ extended: true })); // make post work and have a body
 
 app.use("/", indexRouter);
 
