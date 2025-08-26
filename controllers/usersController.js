8e53fbd676e495d56c0cb1cf639986a776bea1e3
Perry Huang <perryhuang2015@gmail.com>
2025-08-25 20:38:37 -0400

    feat: add user controller for register and login

---
+++ controllers/usersController.js
@@ -0,0 +1,49 @@
+const { body, validationResult, matchedData } = require("express-validator");
+const db = require("../db/queries.js");
+
+const getRegister = (req, res) => {
+    res.render("register", { errors: [] });
+};
+
+const validateUser = [
+    body("username")
+        .trim()
+        .isLength({ min: 1, max: 64 })
+        .withMessage("Username must be between 1 and 64 characters long"),
+    body("firstName")
+        .trim()
+        .isLength({ min: 1, max: 64 })
+        .withMessage("First name must be between 1 and 64 characters long"),
+    body("lastName")
+        .trim()
+        .isLength({ min: 1, max: 64 })
+        .withMessage("Last name must be between 1 and 64 characters long"),
+    body("password")
+        .trim()
+        .isLength({ min: 1, max: 255 })
+        .withMessage("Password must be between 1 and 255 characters long"),
+];
+
+const postRegister = [
+    validateUser,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.render("register", { errors: errors.array() });
+        }
+        const results = matchedData(req);
+        await db.createUser(
+            results.username,
+            results.firstName,
+            results.lastName,
+            results.password,
+        );
+        res.redirect("/login");
+    },
+];
+
+const getLogin = (req, res) => {
+    res.render("login", { errors: [] });
+};
+
+module.exports = {};
