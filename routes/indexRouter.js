0fb0b81170c800e61e107490f65ba7198517fdd9
Perry Huang <perryhuang2015@gmail.com>
2025-08-25 21:51:59 -0400

    feat: add login and register routes

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,24 @@
 const { Router } = require("express");
-
 const indexRouter = Router();
+const postsController = require("../controllers/postsController");
+const usersController = require("../controllers/usersController");
+const passportController = require("../controllers/passportController");
 
-indexRouter.get("/", (req, res) => {
-    res.render("index");
+indexRouter.get("/login", usersController.getLogin);
+indexRouter.post(
+    "/login",
+    passportController.authenticate("local", {
+        failureRedirect: "/login",
+        successRedirect: "/",
+    }),
+);
+indexRouter.get("/register", usersController.getRegister);
+indexRouter.post("/register", usersController.postRegister);
+
+indexRouter.get("/", postsController.getPosts);
+indexRouter.post("/", postsController.createPost);
+indexRouter.get("/{*splat}", (req, res) => {
+    res.redirect("/");
 });
 
 module.exports = indexRouter;
